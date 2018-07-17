457c4edf799b81c96fd85476e2c7b43bfaed91ef
achateigner <aurelien.chateigner@gmail.com>
2018-07-17 13:18:22 +0200

    modified the example again

--- R/anyLib.R
+++ R/anyLib.R
@@ -22,7 +22,8 @@
 #' # Install and load 1 package from a local source file, which name is in an object:
 #' lib <- normalizePath(tempdir(), "/")
 #' listOfPackages <- system.file("dummyPackage_0.1.0.tar.gz", package="anyLib")
-#' anyLib(listOfPackages, force = TRUE, autoUpdate = FALSE, lib = lib, source = TRUE)
+#' anyLib(listOfPackages, force = TRUE, autoUpdate = FALSE, lib = lib,
+#' loadLib = lib, source = TRUE)
 #' @export
 anyLib <- function(pkg, force = FALSE, autoUpdate = TRUE, lib = .libPaths()[1],
                    loadLib = .libPaths(), source = FALSE){
